b33c6006c27d9a46a1d7df7a83665e3457808d5e
Ramanantoanina <rmtandy@gmail.com>
2022-04-05 22:57:57 +0300

    Template sur Exercice1 et Exercice4

--- src/main/java/mg/ramanantoanina/tp2jakarta/jsf/Bean2.java
+++ src/main/java/mg/ramanantoanina/tp2jakarta/jsf/Bean2.java
@@ -41,5 +41,8 @@ public class Bean2 {
     public String afficher() {
     return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
     }
+    public String afficherTemplate() {
+    return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
     
 }
